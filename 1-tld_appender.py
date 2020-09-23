3021c6e80d3dd255dd12158198690bb3324cab16
majd <majd@dsp.ac.cn>
2020-09-23 20:12:24 +0800

    9-23-2

--- 1-tld_appender.py
+++ 1-tld_appender.py
@@ -18,20 +18,22 @@ output:
 
 def max_match(domain, tlds):  # simple match and find the longest match
    match = [i for i in tlds if i in domain] 
-   if len(match)>0:
-       for i in sorted(match,key=lambda x: len(x), reverse = True):
+   if len(match) > 0:
+       for i in sorted(match,key=lambda x: len(x), reverse=True):
            if i == domain[-(len(i)):]:  # longest and matches the end of the domain
                return i
    else: return 'NONE'
 
 
 tld_file = open('1-tld_list.txt', 'r')
+print('tld_file = open(\'1-tld_list.txt\', \'r\')')
 tld_list = list('.'+t.strip().strip('.')+'.' for t in tld_file)  # for domain match, add dot as prefix and postfix
 tld_file.close()
 
 # fi = open('1393459321.nps_malware_dga_training_set.txt','r')
 # fi = open('expanded_training_set.txt','r')
 fi = open('1-conficker_alexa_training.txt', 'r')
+print('fi = open(\'1-conficker_alexa_training.txt\', \'r\')')
 
 fw = open('1-training_w_tld.txt', 'w')
 
@@ -39,6 +41,7 @@ for f in fi:
     domain, cla = f.strip().split('\t')  # # domain为训练集域名，cla为对应标签（恶意类别）
     match = max_match(domain, tld_list)
     fw.write('%s\t%s\t%s\n'%(domain,cla,match))
+print('output:1-training_w_tld.txt, domain, cla, tld')
 
 fw.close()
 fi.close()
